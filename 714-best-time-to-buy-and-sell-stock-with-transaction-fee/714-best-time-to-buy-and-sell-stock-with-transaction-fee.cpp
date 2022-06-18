d903fc83a208c2c910fcdf377c961203173cd8f2
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-06-19 01:06:23 +0530

    Time: 1655 ms (5.04%), Space: 153 MB (5.44%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int dp[100001][2];
+    map<pair<int,int>, int>mp;
     int solve(vector<int>&v, int fee, int i, int buy){
         if(i>=v.size())
             return 0;
         
-        if(dp[i][buy]!=-1)
-            return dp[i][buy];
+        if(mp.find({i,buy})!=mp.end())
+            return mp[{i,buy}];
         
         if(buy){
             int x = solve(v, fee, i+1, 0)+v[i]-fee;
             int y = solve(v, fee, i+1,  1);
-            return dp[i][buy] = max(x,y);
+            return mp[{i,buy}] = max(x,y);
         }
         else{
             int x = solve(v, fee, i+1, 1)-v[i];
             int y = solve(v, fee, i+1, 0);
-            return dp[i][buy] = max(x, y);
+            return mp[{i,buy}] = max(x, y);
         }
     }
     int maxProfit(vector<int>& prices, int fee) {
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
         return solve(prices, fee, 0, 0);
     }
 };
