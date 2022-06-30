fd9714685028ebdc0815343bd02b926bc43be406
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-06-30 19:58:18 +0530

    Time: 99 ms (59.95%), Space: 7.3 MB (71.62%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,31 +2,29 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         string res="";
-        int l=0, r=s.size()-1;
-        int resLen = 0;
-        
-        for(int i=0; i<s.length(); i++){
-            l=i, r=i;
-            while(l>=0 && r<s.length() && s[l]==s[r]){
-                if(r-l+1>resLen){
-                    res.clear();
+        int len=0;
+        for(int i=0; i<s.size(); i++){
+            int l=i,r=i;
+            while(l>=0 and r<s.size() and s[l]==s[r]){
+                if(r-l+1>len){
+                    res.erase();
                     for(int i=l; i<=r; i++)
                         res.push_back(s[i]);
-                    resLen = r-l+1;
+                    len = r-l+1;
                 }
                 l--; r++;
             }
-            l=i, r=i+1;
-            while(l>=0 && r<s.length() && s[l]==s[r]){
-                if(r-l+1>resLen){
-                    res.clear();
+            l = i; r = i+1;
+            while(l>=0 and r<s.size() and s[l]==s[r]){
+                if(r-l+1>len){
+                    res.erase();
                     for(int i=l; i<=r; i++)
                         res.push_back(s[i]);
-                    resLen = r-l+1;
+                    len = r-l+1;
                 }
                 l--; r++;
             }
         }
-      return res;  
+        return res;
     }
 };
