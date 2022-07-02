5399573bc5c29d3b5b0b069bf7c53dc6b5adbf57
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-07-02 22:52:48 +0530

    Time: 29 ms (8.70%), Space: 7.4 MB (45.50%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int>mp1;
+        map<char, int>mp2;
+        for(auto it:s){
+            mp1[it]++;
+        }
+        for(auto it:t){
+            mp2[it]++;
+        }
+        return (mp1==mp2);
+    }
+};
