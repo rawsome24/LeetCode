7f54ffa22a997fa9c7c3f551c84b056203386850
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-07-31 18:25:19 +0530

    Time: 65 ms (45.80%), Space: 18.9 MB (93.98%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
-    
-    struct myCmp{
-      
-        bool operator()(pair<int,int>&a, pair<int,int>&b)
-            {return a.first<b.first;}
-        
-    };
-    
-    vector<vector<int>> merge(vector<vector<int>>& iv) {
-        
-        vector<pair<int,int>>p(iv.size());
-        
-        for(int i=0; i<iv.size(); i++)
-        {
-            p[i].first = iv[i][0];
-            p[i].second = iv[i][1];
-        }
-        
-        sort(p.begin(),p.end(),myCmp());
-        
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
-        
-        vector<int> v(2);
-        v[0] = p[0].first;
-        v[1] = p[0].second;
-        
-        int start = 0;
-        
-        for(int i=1; i<p.size(); i++)
-        {
-            if(v[1]>=p[i].first)
-                v[1] = max(v[1], p[i].second);
-            else
-            {
-                ans.push_back(v);
-                v[0] = p[i].first;
-                v[1] = p[i].second;
+        vector<int>temp = intervals[0];
+        for(int i=1; i<intervals.size(); i++){
+            if(temp[1]>=intervals[i][0]){
+                temp[1] = max(temp[1], intervals[i][1]);
+                // ans.push_back(temp);
+            }
+            else{
+                ans.push_back(temp);
+                temp = intervals[i];
             }
         }
-        ans.push_back(v);
+        ans.push_back(temp);
         return ans;
-        
-        
     }
 };
