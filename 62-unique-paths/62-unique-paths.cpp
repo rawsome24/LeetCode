d3dcdd535174f837fafce7572b79bef30d64594a
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-01 10:05:29 +0530

    Time: 2 ms (50.13%), Space: 6 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int dp[101][101];
-    int solve(vector<vector<int>>&grid, int m, int n){
-        for(int k=0; k<m; k++){
-            dp[k][n-1] = 1;
-        }
-        for(int l=0; l<n; l++){
-            dp[m-1][l] = 1;
-        }
-        
-        for(int i=m-2; i>=0; i--){
-            for(int j=n-2; j>=0; j--){
-                dp[i][j] = dp[i+1][j]+dp[i][j+1];
-            }
-        }
-        return dp[0][0];
+    int solve(int m, int n, int i, int j){
+        if(i<0 or i>=m or j<0 or j>=n)
+            return 0;
+        if(i==m-1 and j==n-1)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=solve(m, n, i+1, j)+solve(m, n, i, j+1);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>grid(m, vector(n, 0));
-        memset(dp,0, sizeof(dp));
-        return solve(grid, m, n);
+        memset(dp, -1, sizeof(dp));
+        return solve(m, n, 0, 0);
     }
 };
