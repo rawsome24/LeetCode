db2a6e261647c6809eceb61064bb8140f648cade
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-03 20:28:55 +0530

    Time: 17 ms (12.51%), Space: 18 MB (10.15%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    void solve(int n, vector<string>&ans, string v, int open, int close){
-        if(open==n && close==n){
-            ans.push_back(v);
+    void solve(int n, vector<string>&ans, string temp, int open){
+        if(temp.size()==n){
+            if(open==0)
+                ans.push_back(temp);
             return;
         }
+        if(open==0){
+            temp.push_back('(');
+            solve(n, ans, temp, open+1);
+            // temp.pop_back();
+        }
+        else if(open==n/2){
+            temp.push_back(')');
+            solve(n, ans, temp, open-1);
+            // temp.pop_back();
+        }
         
-        if(open<n){
-            v.push_back('(');
-            solve(n, ans, v, open+1, close);
-            v.pop_back();
+        
+        else{
+            if(open<=n/2 and open>0){
+                solve(n, ans, temp+"(", open+1);
+                solve(n, ans, temp+")", open-1);
+            }
+            // temp.pop_back();
         }
-        if(close<open){
-            v.push_back(')');
-            solve(n, ans, v, open, close+1);
-            v.pop_back();
-        }
+        
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        int cnt = 0;
-        solve(n, ans, "", 0, 0);
+        vector<string>ans;
+        solve(2*n, ans, "", 0);
         return ans;
     }
 };
