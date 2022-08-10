b5c85334f59c6b50b713202afd1454e26057d288
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-10 09:34:46 +0530

    Time: 7 ms (53.86%), Space: 11.1 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -10,19 +10,15 @@ public:
             if(target == arr[mid])
                 return mid;
             
-            else if(arr[low]<=arr[mid])
-            {
-                if(target>=arr[low] && target<arr[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            }
-            else
-            {
-                if(target<=arr[high] && arr[mid]<target)
+            else if(arr[mid]>arr[high] and target<=arr[high])
+               low = mid+1;
+            else if(arr[mid]<=arr[high] and target>arr[high])
+                high = mid-1;
+            else{
+                if(arr[mid]>target)
+                    high = mid-1;
+                else if(arr[mid]<target)
                     low = mid+1;
-                else
-                    high = mid - 1;
             }
         }
         return -1;
