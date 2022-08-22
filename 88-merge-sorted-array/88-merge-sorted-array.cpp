077d21ea13c9221d88134ff2ca2fb1f47d724f79
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-22 15:17:56 +0530

    Time: 5 ms (40.86%), Space: 9 MB (72.88%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>ans;
-        int first = 0, second = 0;
-        while(first<m && second<n){
-            if(nums1[first]<=nums2[second]) {ans.push_back(nums1[first]); first++;}
-            else if(nums1[first]>nums2[second]){ans.push_back(nums2[second]); second++;}
+        if(n==0)
+            return;
+        if(m==0){
+            nums1 = nums2;
+            return;
         }
-        if(second==n){
-            for(int i=first; i<m; i++)
-                ans.push_back(nums1[i]);
+        int first = 0, second = 0, last = m;
+        while(first<last and second<n){
+            if(nums1[first]>nums2[second]){
+                for(int i=m+n-1; i>=first+1; i--)
+                    nums1[i] = nums1[i-1];
+                nums1[first] = nums2[second];
+                first++;
+                second++;
+                last++;
+            }
+            else{
+                first++;
+            }
+            // for(int i=0; i<m+n; i++)
+            //     cout<<nums1[i]<<" ";
+            // cout<<endl;
         }
-        if(first==m){
-            for(int i=second; i<n; i++)
-                ans.push_back(nums2[i]);
+        while(second<n){
+            nums1[first] = nums2[second];
+            first++; second++;
         }
-        nums1=ans;
     }
 };
