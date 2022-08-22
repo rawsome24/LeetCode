38f427236d3d9bbfcc8c6954eef74597af306e75
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-22 14:33:00 +0530

    Time: 41 ms (91.48%), Space: 18.9 MB (85.11%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,11 +4,10 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
         vector<int>temp = intervals[0];
-        for(int i=1; i<intervals.size(); i++){
+        for(int i=0; i<intervals.size(); i++){
             if(temp[1]>=intervals[i][0]){
                 temp[1] = max(temp[1], intervals[i][1]);
-            }
-            else{
+            }else{
                 ans.push_back(temp);
                 temp = intervals[i];
             }
