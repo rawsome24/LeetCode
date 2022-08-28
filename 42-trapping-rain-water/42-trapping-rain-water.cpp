d02efc0517704d88b217e1d5d6c1e316cdf4998a
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-08-28 10:17:24 +0530

    Time: 21 ms (70.07%), Space: 20.5 MB (36.96%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int res = 0;
-        int lMax[h.size()];
-        lMax [0] = h[0];
-        for(int i=1; i<h.size(); i++){
-            lMax[i] = max(lMax[i-1], h[i]);
+    int trap(vector<int>& height) {
+        vector<int>lMax(height.size(), 0);
+        vector<int>rMax(height.size(), 0);
+        lMax[0] = height[0];
+        for(int i=1; i<height.size(); i++)
+            lMax[i] = max(lMax[i-1], height[i]);
+        rMax[height.size()-1] = height[height.size()-1];
+        for(int i=height.size()-2; i>=0; i--)
+            rMax[i] = max(rMax[i+1], height[i]);
+        long long ans =0;
+        for(int i=1; i<height.size()-1; i++){
+            ans += min(lMax[i], rMax[i])-height[i];
         }
-        int rMax[h.size()];
-        rMax[h.size()-1] = h[h.size()-1];
-        for(int i=h.size()-2; i>=0; i--){
-            rMax[i] = max(rMax[i+1], h[i]);
-        }
-        for(int i=1; i<h.size()-1; i++){
-            res+=min(lMax[i],rMax[i])-h[i];
-        }
-        return res;
+        return ans;
     }
 };
