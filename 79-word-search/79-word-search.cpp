27276416726735862a3a71c59995ad1e6a4b0458
Somesh Rawat <95746772+rawsome24@users.noreply.github.com>
2022-09-12 22:45:56 +0530

    Time: 1917 ms (5.01%), Space: 8.1 MB (46.01%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>&v, string word, int i, int j, int m, int n, int index){
-        if (index==word.size())
+    bool solve(vector<vector<char>>&v, int m, int n, string word, string temp, int i, int j, int k){
+        if(k==word.length())
             return true;
-        if(i<0 or j<0 or i>=m or j>=n){
+        if(i<0 or j<0 or i>=m or j>=n or k>=word.length())
             return false;
-        }
-        if(v[i][j]!=word[index])
+        if(v[i][j]!=word[k])
             return false;
-        v[i][j]='*';
-        bool search = solve(v, word, i+1, j, m, n, index+1) or
-                        solve(v, word, i, j+1, m, n, index+1) or
-                            solve(v, word, i-1, j, m, n, index+1) or
-                                solve(v, word, i, j-1, m, n, index+1);
-        v[i][j]=word[index];
-        return search;
-        
+        v[i][j] = '*';
+        bool flag = solve(v, m, n, word, temp, i+1, j, k+1)
+                        or solve(v, m, n, word, temp, i, j+1, k+1) 
+                            or solve(v, m, n, word, temp, i-1, j, k+1) 
+                                or solve(v, m, n, word, temp, i, j-1, k+1);
+        v[i][j] = word[k];
+        return flag;
     }
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        string ans="";
-        vector<vector<int>>vis(m,vector<int>(n, 0));
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(word.size()==1){
-                    string s(1,board[i][j]);
-                    if(word==s)
-                        return true;
-                }
-                else{
-                    if(solve(board, word, i, j, m, n, 0))
-                        return true;
-                }
-            }
+        string temp = "";
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++)
+               if(solve(board, board.size(), board[0].size(), word, "", i, j, 0))
+                   return true;
         }
         return false;
     }
